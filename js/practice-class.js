623dea9b4c68157c884deb457d36b5be87089c74
hamzamain <mohammadmainuddin211@gmail.com>
2022-09-02 14:13:42 +0600

    class practice

---
+++ js/practice-class.js
@@ -0,0 +1,25 @@
+class TeamMember {
+    name;
+    address;
+    skills;
+    constructor(name, address, skills){
+        this.name = name;
+        this.address = address;
+        this.skills = skills;
+    }
+    shereExperience(){
+        console.log(`${this.name}, thank you for your feedback about experience`)
+    }
+}
+
+class Goli extends TeamMember{
+    were;
+    constructor(name, address, skills, were){
+        super(name, address, skills);
+        this.were =were;
+    }
+}
+
+const anamul = new Goli('Anamul huque', 'cumilla, bangla desh', 'Goli', 'hand glovs');
+console.log(anamul)
+anamul.shereExperience()
