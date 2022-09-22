cfde80c77deae838309f5c5efa1ed328be9ccfef
hamzamain <mohammadmainuddin211@gmail.com>
2022-09-22 15:45:01 +0600

    last

--- js/practice-class.js
+++ js/practice-class.js
@@ -1,25 +1,30 @@
 class TeamMember {
-    name;
-    address;
-    skills;
-    constructor(name, address, skills){
-        this.name = name;
-        this.address = address;
-        this.skills = skills;
-    }
-    shereExperience(){
-        console.log(`${this.name}, thank you for your feedback about experience`)
-    }
+  name;
+  address;
+  skills;
+  constructor(name, address, skills) {
+    this.name = name;
+    this.address = address;
+    this.skills = skills;
+  }
+  shereExperience() {
+    console.log(`${this.name}, thank you for your feedback about experience`);
+  }
 }
 
-class Goli extends TeamMember{
-    were;
-    constructor(name, address, skills, were){
-        super(name, address, skills);
-        this.were =were;
-    }
+class Goli extends TeamMember {
+  were;
+  constructor(name, address, skills, were) {
+    super(name, address, skills);
+    this.were = were;
+  }
 }
 
-const anamul = new Goli('Anamul huque', 'cumilla, bangla desh', 'Goli', 'hand glovs');
-console.log(anamul)
-anamul.shereExperience()
+const anamul = new Goli(
+  "Anamul huque",
+  "cumilla, bangla desh",
+  "Goli",
+  "hand glovs"
+);
+console.log(anamul);
+anamul.shereExperience();
